b070d362c35bf7ffb7bc881f90567d74161e5d1f
aloyschen <813971830@qq.com>
2017-10-10 23:20:30 +0800

    update

--- CodeWars_4kyu.py
+++ CodeWars_4kyu.py
@@ -24,9 +24,37 @@ def convert(input, source, target):
         acc = acc/base_out
         out = d + out
     return out if out else target[0]
+"""
+该函数用于将时间转换为对人友好的形式
+Param seconds: 代表时间的非0整数
+"""
+def format_duration(seconds):
+    times = [("year", 365 * 24 * 60 * 60),
+             ("day", 24 * 60 * 60),
+             ("hour", 60 * 60),
+             ("minute", 60),
+             ("second", 1)]
+
+    if not seconds:
+        return "now"
+
+    chunks = []
+    for name, secs in times:
+        qty = seconds // secs
+        if qty:
+            if qty > 1:
+                name += "s"
+            chunks.append(str(qty) + " " + name)
+
+        seconds = seconds % secs
+
+    return ', '.join(chunks[:-1]) + ' and ' + chunks[-1] if len(chunks) > 1 else chunks[0]
+
+
 if __name__ == "__main__":
     # print(convert("abc", allow, hex))
     a = np.zeros((2, 2, 3))
     b = np.ones((2, 2, 3))
     print("a ====", a, "\n", "b ====", b)
     print(np.concatenate((a, b), axis = 2))
+    print(format_duration(3600))
