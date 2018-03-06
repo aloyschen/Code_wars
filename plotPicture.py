7c9a9b24dcff9c331d4fedb888ae04f34fe7eccc
aloyschen <813971830@qq.com>
2018-03-06 11:51:47 +0800

    update

--- plotPicture.py
+++ plotPicture.py
@@ -17,7 +17,7 @@ def autoLabel(rect):
         height = rect.get_height()
         plt.text(rect.get_x() + rect.get_width()/2. - 0.48, 1.015 * height, '%s' % float(height))
 
-def plot():
+def plot_watchTime():
     """
     该函数根据用户的观看时间统计画出分布直方图
     Parameters
@@ -42,5 +42,46 @@ def plot():
     plt.xticks(range(11))
     plt.show()
 
+
+def plot_increment():
+    """
+    该函数是统计增量数据画出双柱状图
+    Parameters
+    ----------
+        None
+    Returns
+    -------
+        None
+    """
+    data1, data2 = dict(), dict()
+    for line in open('./increment_little', encoding = 'utf-8'):
+        line = line.strip().split(',')
+        data1[line[0]] = [line[1], line[2]]
+    for line in open('./increment_large', encoding = 'utf-8'):
+        line = line.strip().split(',')
+        data2[line[0]] = [line[1], line[2]]
+    x1 = range(3)
+    x2 = range(4)
+    relation1 = [int(data1[key][0]) / 10000 for key in data1.keys()]
+    total1 = [int(data1[key][1]) / 10000 for key in data1.keys()]
+    relation2 = [int(data2[key][0]) / 10000 for key in data2.keys()]
+    total2 = [int(data2[key][1]) / 10000 for key in data2.keys()]
+    # plt.subplot(121)
+    rect1 = plt.bar(x1, relation1, width = 0.3)
+    rect2 = plt.bar(x1, total1, bottom = relation1, width = 0.3)
+    plt.xlabel(u'用户行为', fontproperties = font)
+    plt.ylabel(u'用户人数(单位：万)', fontproperties = font)
+    plt.title(u'各种行为非关注关系的增量统计', fontproperties = font)
+    plt.xticks(x1, data1.keys(), fontproperties = font)
+    # plt.subplot(122)
+    # rect3 = plt.bar(x2, relation2, width = 0.5)
+    # rect4 = plt.bar(x2, total2, bottom = relation2, width = 0.5)
+    # plt.xlabel(u'用户行为', fontproperties = font)
+    # plt.ylabel(u'用户人数(单位：万)', fontproperties = font)
+    # plt.title(u'各种行为非关注关系的增量统计', fontproperties = font)
+    # plt.xticks(x2, data2.keys(), fontproperties = font)
+    plt.legend((u'满足关注关系人数', u'非关注关系的增量人数'), prop = font)
+    plt.show()
+
 if __name__ == '__main__':
-    plot()
+    plot_increment()
